df0359557def348db7275484ef30aba46fa253bb
Tanjin Panna <tanjin.panna@gmail.com>
2018-01-20 23:27:53 -0500

    simple algorithm for bulk insert (millions) due to limit on postgres end (~32K) limitation is local memory, got java out of memory exception with 3 million case classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,20 +9,43 @@ import com.typesafe.config.ConfigFactory
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val companies = TableQuery[Companies]
+    val companyTable = TableQuery[Companies]
+    val propertyTable = TableQuery[Properties]
+    val floorTable = TableQuery[Floors]
+    val spaceTable = TableQuery[Spaces]
+    val dealTable = TableQuery[Deals]
 
-    val url = ConfigFactory.load().getString("DB_URL")
+    val config = ConfigFactory.load
+
+    val url = config.getString("DB_URL")
     val driver = "org.postgresql.Driver"
-    val user = ConfigFactory.load().getString("DB_USER")
-    val pass = ConfigFactory.load().getString("DB_PASSWORD")
+    val user = config.getString("DB_USER")
+    val pass = config.getString("DB_PASSWORD")
 
     val db = Database.forURL(url = url, driver = driver, user = user, password = pass)
 
+    val companyCount = 3000
+    val propertyCount = 100000
+    val floorCount = 1000000
+    val spaceCount = 3000000
+    val dealCount = 5000000
 
-    try{
-      Await.result(db.run(DBIO.seq(
-        companies.result.map(println)
-      )), Duration.Inf)
-    } finally db.close
+    val allSpaces = for {
+      s <- 2000001 to spaceCount
+      f = math.ceil(s.toDouble/floorCount).toInt
+    } yield Space(s, f, s"space_${f}_$s")
+
+    // to get around bulk upload limit, creating an array of insert commands which is then put into DBIO.seq
+    // insert commands are done via the .grouped (
+    // limit is 32678 (I think - just did 25K for nice even round number
+    val grouped = allSpaces.grouped(25000).toList
+    val sqlCommand = for {
+      index <- grouped.indices
+    } yield spaceTable ++= grouped(index)
+
+    Await.result(db.run(DBIO.seq(
+      sqlCommand:_*
+    )), Duration.Inf)
+
   }
 }
