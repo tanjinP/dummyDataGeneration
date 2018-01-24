8c515a00113b345e4c49577c64ad1361b6798d78
Tanjin Panna <tanjin.panna@gmail.com>
2018-01-24 13:11:39 -0500

    implemented logic to generate activity logs with some generics can build further but will 'hard-code' generators for now - can look into ScalaCheck / shapeless

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,51 +1,60 @@
+import java.sql.{Date, Timestamp}
+
 import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import scala.concurrent.ExecutionContext.Implicits.global
-
 import com.typesafe.config.ConfigFactory
 
 object Main {
 
+  private val HALF_MILLION = 500000
+  private val CHUNK_AMOUNT = 10000
+
   def main(args: Array[String]): Unit = {
-    val companyTable = TableQuery[Companies]
-    val propertyTable = TableQuery[Properties]
-    val floorTable = TableQuery[Floors]
-    val spaceTable = TableQuery[Spaces]
-    val dealTable = TableQuery[Deals]
 
+    // obtaining details of database from application.conf
     val config = ConfigFactory.load
-
     val url = config.getString("DB_URL")
     val driver = "org.postgresql.Driver"
     val user = config.getString("DB_USER")
     val pass = config.getString("DB_PASSWORD")
 
+    // connecting to database to insert dummy data into (currently postgres of AWS RDS)
     val db = Database.forURL(url = url, driver = driver, user = user, password = pass)
 
-    val companyCount = 3000
-    val propertyCount = 100000
-    val floorCount = 1000000
-    val spaceCount = 3000000
-    val dealCount = 5000000
+    // generating lots of case class reprentation of table data via for comp (see def)
+    val activities = generateActivityLogs
+    println("successfully generated dummy case classes")
 
-    val allSpaces = for {
-      s <- 2000001 to spaceCount
-      f = math.ceil(s.toDouble/floorCount).toInt
-    } yield Space(s, f, s"space_${f}_$s")
-
-    // to get around bulk upload limit, creating an array of insert commands which is then put into DBIO.seq
-    // insert commands are done via the .grouped (
-    // limit is 32678 (I think - just did 25K for nice even round number
-    val grouped = allSpaces.grouped(25000).toList
+    // chunking the seq so insertion into postgres can be done without bulk exception being thrown (limit is ~37K items)
+    val chunkedActivities = chunk(activities).toList
+    val activityLogsTable = TableQuery[ActivityLogs]
     val sqlCommand = for {
-      index <- grouped.indices
-    } yield spaceTable ++= grouped(index)
+      index <- chunkedActivities.indices
+    } yield activityLogsTable ++= chunkedActivities(index)
 
+    // executing SQL command to insert into database in batch inserts
     Await.result(db.run(DBIO.seq(
       sqlCommand:_*
     )), Duration.Inf)
 
+  }
+
+  // def has potential to be generic with reflexion regarding generation of fields - won't dig into
+  private def generateActivityLogs: IndexedSeq[ActivityLog] = {
+    for {
+      id <- 1 to HALF_MILLION
+      string = s"here's a random string with the id number $id"
+      created = new Timestamp((math.random * 100000000 * id).toLong)
+      updated = new Timestamp((math.random * 600000000 * id).toLong)
+      userId = math.ceil(id.toDouble/5000).toInt
+      date = new Date(id/100000)
+    } yield ActivityLog(id, created, updated, string, userId, string, string, id, date, created, id, date)
+  }
+
+
+  private def chunk[A](seq: Seq[A]): Iterator[Seq[A]] = {
+    seq.grouped(CHUNK_AMOUNT)
   }
 }
