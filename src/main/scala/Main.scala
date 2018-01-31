17c53a3e764f5639d4c5ec63931fbeb4c0badca1
Tanjin Panna <tanjin.panna@gmail.com>
2018-01-31 05:24:45 -0500

    added more dummy generation - cut down more complicated data generation like dates

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,12 @@
-import java.sql.{Date, Timestamp}
-
+import com.typesafe.config.ConfigFactory
 import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import com.typesafe.config.ConfigFactory
 
 object Main {
 
-  private val HALF_MILLION = 500000
+  private val RECORDS_TO_INSERT = 500000
   private val CHUNK_AMOUNT = 10000
 
   def main(args: Array[String]): Unit = {
@@ -23,20 +21,27 @@ object Main {
     // connecting to database to insert dummy data into (currently postgres of AWS RDS)
     val db = Database.forURL(url = url, driver = driver, user = user, password = pass)
 
-    // generating lots of case class reprentation of table data via for comp (see def)
+    // generating lots of case class representing table data via for comp (see def)
     val activities = generateActivityLogs
-    println("successfully generated dummy case classes")
-
     // chunking the seq so insertion into postgres can be done without bulk exception being thrown (limit is ~37K items)
     val chunkedActivities = chunk(activities).toList
     val activityLogsTable = TableQuery[ActivityLogs]
-    val sqlCommand = for {
+    val activityInsertSql = for {
       index <- chunkedActivities.indices
     } yield activityLogsTable ++= chunkedActivities(index)
 
+    val proposals = generateProposals
+
+    val chunkedProposals = chunk(proposals).toList
+    val proposalTable = TableQuery[Proposals]
+    val proposalInsertSql = for {
+      index <- chunkedProposals.indices
+    } yield proposalTable ++= chunkedProposals(index)
+
+    val sql = activityInsertSql ++ proposalInsertSql
     // executing SQL command to insert into database in batch inserts
     Await.result(db.run(DBIO.seq(
-      sqlCommand:_*
+      sql:_*
     )), Duration.Inf)
 
   }
@@ -44,13 +49,19 @@ object Main {
   // def has potential to be generic with reflexion regarding generation of fields - won't dig into
   private def generateActivityLogs: IndexedSeq[ActivityLog] = {
     for {
-      id <- 1 to HALF_MILLION
+      id <- RECORDS_TO_INSERT + 1 to 2 * RECORDS_TO_INSERT
       string = s"here's a random string with the id number $id"
-      created = new Timestamp((math.random * 100000000 * id).toLong)
-      updated = new Timestamp((math.random * 600000000 * id).toLong)
       userId = math.ceil(id.toDouble/5000).toInt
-      date = new Date(id/100000)
-    } yield ActivityLog(id, created, updated, string, userId, string, string, id, date, created, id, date)
+    } yield ActivityLog(id, string, userId, string, string, id, id)
+  }
+
+  private def generateProposals: IndexedSeq[Proposal] = {
+    for {
+      id <- RECORDS_TO_INSERT + 1 to 2 * RECORDS_TO_INSERT
+      string = s"string for the proposal $id"
+      activityId = math.ceil(id.toDouble/5000).toInt
+      bool = id %2 == 0
+    } yield Proposal(id, activityId, bool, !bool, string)
   }
 
 
--- src/main/scala/Tables.scala
+++ src/main/scala/Tables.scala
@@ -1,35 +1,37 @@
 import slick.driver.PostgresDriver.api._
 
-import java.sql.{Date, Timestamp}
-
 case class ActivityLog(
                       id: Int,
-                      createdAt: Timestamp,
-                      updatedAt: Timestamp,
                       status: String,
                       userId: Int,
                       industryName: String,
                       tenantLocation: String,
                       successProbability: Int,
-                      leaseExpiration: Date,
-                      lastModifiedTime: Timestamp,
-                      accountId: Int,
-                      moveInDate: Date
+                      accountId: Int
                       )
-
 case class ActivityLogs(tag: Tag) extends Table[ActivityLog](tag, "activity_logs") {
   def id = column[Int]("id")
-  def createdAt = column[Timestamp]("created_at")
-  def updatedAt = column[Timestamp]("updated_at")
   def status = column[String]("status")
   def userId = column[Int]("user_id")
   def industryName = column[String]("industry_name")
   def tenantLocation = column[String]("tenant_location")
   def successProbability = column[Int]("success_probability")
-  def leaseExpiration = column[Date]("lease_expiration_date")
-  def lastModifiedTime = column[Timestamp]("last_modified_time")
   def accountId = column[Int]("account_id")
-  def moveInDate = column[Date]("move_in_date")
-  def * = (id, createdAt, updatedAt, status, userId, industryName, tenantLocation, successProbability,
-    leaseExpiration, lastModifiedTime, accountId, moveInDate) <> (ActivityLog.tupled, ActivityLog.unapply)
+  def * = (id, status, userId, industryName, tenantLocation, successProbability, accountId) <> (ActivityLog.tupled, ActivityLog.unapply)
+}
+
+case class Proposal(
+                   id: Int,
+                   activityLogId: Int,
+                   lastEntered: Boolean,
+                   editLocked: Boolean,
+                   proposalType: String
+                   )
+case class Proposals(tag: Tag) extends Table[Proposal](tag, "proposals") {
+  def id = column[Int]("id")
+  def activityLog = column[Int]("activity_log_id")
+  def lastEntered = column[Boolean]("last_entered")
+  def editLocked = column[Boolean]("edit_locked")
+  def proposalType = column[String]("proposal_type")
+  def * = (id, activityLog, lastEntered, editLocked, proposalType) <> (Proposal.tupled, Proposal.unapply)
 }
